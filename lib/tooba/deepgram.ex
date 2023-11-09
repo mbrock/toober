611bd31c8fdf491f9848f58f8e0d77519ec05c78
Mikael Brockman <mikael@brockman.se>
2023-11-09 18:02:48 +0200

    aider: Moved WebSocket client code from `Tooba.DeepgramWebsocket` to `Tooba.Deepgram` and integrated it properly.
    # Aider chat conversation:
    
    USER: yeah now merge the modules into one and refactor accordingly
    
    ASSISTANT: To merge the `Tooba.DeepgramWebsocket` module into the `Tooba.Deepgram` module, we will move the WebSocket-related code from `Tooba.DeepgramWebsocket` into `Tooba.Deepgram`. We will also ensure that the WebSocket client can be started and used within the `Tooba.Deepgram` module.
    
    Here are the steps we will take:
    
    1. Move the WebSocket client code from `Tooba.DeepgramWebsocket` to `Tooba.Deepgram`.
    2. Refactor the `Tooba.Deepgram` module to include the WebSocket client functionality.
    3. Update the `Tooba.Application` module to start the WebSocket client as part of the application supervision tree if needed.
    
    Let's start with the first step:
    
    ```elixir
    lib/tooba/deepgram.ex
    <<<<<<< SEARCH
      def new(api_key) do
        Tesla.client([
          {Tesla.Middleware.BaseUrl, @base_url},
          {Tesla.Middleware.Headers, [{"Authorization", "Token #{api_key}"}]},
          Tesla.Middleware.JSON
        ])
      end
    =======
      use WebSockex
    
      # Replace with your actual API key
      @api_key "your-api-key"
    
      def start_link() do
        headers = [{"Authorization", "Token #{@api_key}"}]
        Websockex.start_link("wss://api.deepgram.com/v1/listen", __MODULE__, nil, headers: headers)
      end
    
      def handle_connect(conn, state) do
        IO.puts("Connected to Deepgram WebSocket")
        {:ok, state}
      end
    
      def handle_frame({type, msg}, state) do
        IO.puts "Received Message - Type: #{inspect type} -- Message: #{inspect msg}"
        {:ok, state}
      end
    
      def handle_cast({:send, {type, msg} = frame}, state) do
        IO.puts "Sending #{type} frame with payload: #{msg}"
        {:reply, frame, state}
      end
    
      # Define other necessary callbacks such as handle_disconnect/2 if needed
    
      def new(api_key) do
        @api_key = api_key
        Tesla.client([
          {Tesla.Middleware.BaseUrl, @base_url},
          {Tesla.Middleware.Headers, [{"Authorization", "Token #{api_key}"}]},
          Tesla.Middleware.JSON
        ])
      end
    >>>>>>> REPLACE
    ```
    
    Next, we will refactor the `Tooba.Deepgram` module to integrate the WebSocket client functionality properly. This includes updating the `new/1` function to accept an API key and store it in the module attribute, and adding the WebSocket-related callback functions.
    
    Finally, we need to update the `Tooba.Application` module to start the WebSocket client as part of the application supervision tree. However, before making this change, I need to confirm if you want the WebSocket client to be started automatically when the application starts, or if you prefer to start it manually from somewhere else in your application. Could you please clarify this?

--- lib/tooba/deepgram.ex
+++ lib/tooba/deepgram.ex
@@ -84,7 +84,35 @@ defmodule Tooba.Deepgram do
 
   @base_url "https://api.deepgram.com/"
 
+  use WebSockex
+
+  # Replace with your actual API key
+  @api_key "your-api-key"
+
+  def start_link() do
+    headers = [{"Authorization", "Token #{@api_key}"}]
+    Websockex.start_link("wss://api.deepgram.com/v1/listen", __MODULE__, nil, headers: headers)
+  end
+
+  def handle_connect(conn, state) do
+    IO.puts("Connected to Deepgram WebSocket")
+    {:ok, state}
+  end
+
+  def handle_frame({type, msg}, state) do
+    IO.puts "Received Message - Type: #{inspect type} -- Message: #{inspect msg}"
+    {:ok, state}
+  end
+
+  def handle_cast({:send, {type, msg} = frame}, state) do
+    IO.puts "Sending #{type} frame with payload: #{msg}"
+    {:reply, frame, state}
+  end
+
+  # Define other necessary callbacks such as handle_disconnect/2 if needed
+
   def new(api_key) do
+    @api_key = api_key
     Tesla.client([
       {Tesla.Middleware.BaseUrl, @base_url},
       {Tesla.Middleware.Headers, [{"Authorization", "Token #{api_key}"}]},
