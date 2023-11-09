d1a2889b7988c5265d54f53c892cf27fe545e6a0
Mikael Brockman <mikael@brockman.se>
2023-11-09 18:15:41 +0200

    Refactor Tooba.Record to use a TranscriptionSink module and update child specifications.

--- lib/tooba/record.ex
+++ lib/tooba/record.ex
@@ -1,36 +1,30 @@
 # Let's use Membrane with PortAudio to record audio from the microphone
 # and send it to Deepgram for transcription.
 
+defmodule Tooba.TranscriptionSink do
+  use Membrane.Sink
+
+  def_input_pad(:input, flow_control: :auto, accepted_format: %Membrane.Matroska{})
+end
+
 defmodule Tooba.Record do
   use Membrane.Pipeline
 
   @impl true
-  def handle_init(_) do
-    children = [
-      portaudio: %Membrane.PortAudio.Source{
-        input_device: :default,
+  def handle_init(_, _) do
+    spec =
+      child(%Membrane.PortAudio.Source{
+        endpoint_id: :default,
         sample_rate: 48_000,
         channels: 2,
-        format: :s16le,
-        latency: 20
-      },
-      opus: Membrane.Opus.Encoder,
-      matroska: %Membrane.Matroska.Muxer{
-        output_path: "output.mkv"
-      }
-    ]
+        sample_format: :s16le
+      })
+      |> child(%Membrane.Opus.Encoder{
+        application: :audio
+      })
+      |> child(%Membrane.Matroska.Muxer{})
+      |> child(Tooba.TranscriptionSink)
 
-    links = [
-      link(:portaudio)
-      |> to(:opus)
-      |> to(:matroska)
-    ]
-
-    spec = %Membrane.Pipeline.Spec{
-      children: children,
-      links: links
-    }
-
-    {{:ok, spec}, %{}}
+    {[spec: spec], %{}}
   end
 end
