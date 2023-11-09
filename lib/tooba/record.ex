e4247c00eac6b094d4236dee6e4556f5843039c5
Mikael Brockman <mikael@brockman.se>
2023-11-09 16:25:34 +0200

    aider: Refactored `Tooba.Record` module to define a Membrane pipeline that captures audio from the microphone, encodes it using the Opus codec, and saves it into a Matroska container file.
    # Aider chat conversation:
    
    ASSISTANT: To set up Membrane with PortAudio input through an Opus encoder and a Matroska container in `record.ex`, you'll need to define a Membrane pipeline that captures audio from the microphone, encodes it using the Opus codec, and then saves it into a Matroska container file.
    
    Here's how you can define such a pipeline in `record.ex`:
    
    ```elixir
    lib/tooba/record.ex
    <<<<<<< SEARCH
    defmodule Tooba.Record do
    end
    =======
    defmodule Tooba.Record do
      use Membrane.Pipeline
    
      @impl true
      def handle_init(_) do
        children = [
          portaudio: %Membrane.PortAudio.Source{
            input_device: :default,
            sample_rate: 48_000,
            channels: 2,
            format: :s16le,
            latency: 20
          },
          opus: Membrane.Opus.Encoder,
          matroska: %Membrane.Matroska.Muxer{
            output_path: "output.mkv"
          }
        ]
    
        links = [
          link(:portaudio)
          |> to(:opus)
          |> to(:matroska)
        ]
    
        spec = %Membrane.Pipeline.Spec{
          children: children,
          links: links
        }
    
        {{:ok, spec}, %{}}
      end
    end
    >>>>>>> REPLACE
    ```
    
    This code defines a `Tooba.Record` module that uses the `Membrane.Pipeline` behaviour. It initializes a pipeline with three elements:
    
    1. `Membrane.PortAudio.Source`: Captures audio from the default input device with the specified sample rate, channels, and format.
    2. `Membrane.Opus.Encoder`: Encodes the raw audio into the Opus format.
    3. `Membrane.Matroska.Muxer`: Muxes the encoded audio into a Matroska container and saves it to `output.mkv`.
    
    The `links` define the flow of data from the source to the encoder and then to the muxer.
    
    Please note that you need to have the appropriate Membrane dependencies in your `mix.exs` file for this to work. If you haven't already added them, you'll need to include `membrane_portaudio_plugin`, `membrane_opus_plugin`, and `membrane_matroska_plugin` with compatible versions.
    
    If you need to adjust the configuration options like `sample_rate`, `channels`, or `output_path`, you can modify the values in the `children` list accordingly.
    
    After making these changes, you can start the pipeline by calling `Membrane.Pipeline.start_link(Tooba.Record)` from your application code.

--- lib/tooba/record.ex
+++ lib/tooba/record.ex
@@ -2,4 +2,35 @@
 # and send it to Deepgram for transcription.
 
 defmodule Tooba.Record do
+  use Membrane.Pipeline
+
+  @impl true
+  def handle_init(_) do
+    children = [
+      portaudio: %Membrane.PortAudio.Source{
+        input_device: :default,
+        sample_rate: 48_000,
+        channels: 2,
+        format: :s16le,
+        latency: 20
+      },
+      opus: Membrane.Opus.Encoder,
+      matroska: %Membrane.Matroska.Muxer{
+        output_path: "output.mkv"
+      }
+    ]
+
+    links = [
+      link(:portaudio)
+      |> to(:opus)
+      |> to(:matroska)
+    ]
+
+    spec = %Membrane.Pipeline.Spec{
+      children: children,
+      links: links
+    }
+
+    {{:ok, spec}, %{}}
+  end
 end
