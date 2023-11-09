3ce4b0c9eb5219f9bb34f86a15212d41e0648a35
Mikael Brockman <mikael@brockman.se>
2023-11-09 18:39:13 +0200

    Send audio data to WebSocket client in handle_info callback.

--- lib/tooba/deepgram_sink.ex
+++ lib/tooba/deepgram_sink.ex
@@ -1,24 +1,25 @@
 defmodule Tooba.DeepgramSink do
   use Membrane.Sink
 
+  def_options(
+    deepgram_opts: [
+      spec: any()
+    ]
+  )
+
   alias Tooba.Deepgram
 
   @impl true
-  def handle_init(%{deepgram_opts: deepgram_opts}) do
+  def handle_init(_ctx, %{deepgram_opts: deepgram_opts}) do
     # Start the WebSocket client and keep the pid to send messages later
     {:ok, ws_pid} = Deepgram.start_link(deepgram_opts)
-    {:ok, %{ws_pid: ws_pid}}
+    {[], %{ws_pid: ws_pid}}
   end
 
   @impl true
-  def handle_write(_buffer, %{ws_pid: ws_pid} = state) do
-    # Here you would convert the buffer to the format expected by Deepgram
-    # and send it through the WebSocket connection
-    # This is a placeholder for the actual implementation
-    # Deepgram.send_audio(ws_pid, converted_buffer)
-
-    {:ok, state}
+  def handle_info({:data, data}, _ctx, %{ws_pid: ws_pid} = state) do
+    # Send the audio data to the WebSocket client
+    WebSockex.send_frame(ws_pid, {:binary, data})
+    {[], state}
   end
-
-  # Implement other callbacks such as handle_prepared_to_playing/1 if needed
 end
