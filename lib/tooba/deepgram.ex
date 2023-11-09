e9b0169e2e38052c4b15b2df22c543288834bd97
Mikael Brockman <mikael@brockman.se>
2023-11-09 15:44:14 +0200

    Add RequestParams module to handle request parameters for the Deepgram API.

--- lib/tooba/deepgram.ex
+++ lib/tooba/deepgram.ex
@@ -3,6 +3,41 @@ defmodule Tooba.Deepgram do
   A client for interacting with the Deepgram speech-to-text API.
   """
 
+  defmodule RequestParams do
+    use Ecto.Schema
+
+    embedded_schema do
+      field :model, :string, default: "general"
+      field :tier, :string, default: "base"
+      field :version, :string, default: "latest"
+      field :language, :string, default: "en"
+      field :detect_language, :boolean, default: false
+      field :punctuate, :boolean, default: false
+      field :profanity_filter, :boolean, default: false
+      field :redact, {:array, :string}
+      field :diarize, :boolean, default: false
+      field :diarize_version, :string
+      field :smart_format, :boolean, default: false
+      field :filler_words, :boolean, default: false
+      field :multichannel, :boolean, default: false
+      field :alternatives, :integer, default: 1
+      field :search, {:array, :string}
+      field :replace, {:array, :string}
+      field :callback, :string
+      field :keywords, {:array, :string}
+      field :paragraphs, :boolean, default: false
+      field :summarize, :string, default: "v2"
+      field :detect_topics, :boolean, default: false
+      field :utterances, :boolean, default: false
+      field :utt_split, :float, default: 0.8
+      field :tag, :string
+      field :numerals, :boolean, default: false
+      field :ner, :boolean, default: false
+      field :measurements, :boolean, default: false
+      field :dictation, :boolean, default: false
+    end
+  end
+
   @base_url "https://api.deepgram.com/"
 
   def new(api_key) do
