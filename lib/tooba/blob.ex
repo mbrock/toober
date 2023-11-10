4998f7831e17866b3dcac4b34c94e65192a9f1eb
Mikael Brockman <mikael@brockman.se>
2023-11-10 02:34:14 +0200

    aider: Implement blob.ex using the user data dir like in the rdf store.
    # Aider chat conversation:
    
    USER: let's implement blob.ex also using the user data dir like in the rdf store...
    
    ASSISTANT: To implement `Tooba.Blob.Store` using the user data directory similar to `Tooba.RDF.Store`, we will need to add functions to handle file operations within a designated data directory. We'll create functions to save and retrieve blobs, ensuring that the directory exists before performing any file operations.
    
    Here's how we can modify the `lib/tooba/blob.ex` file:
    
    ```elixir
    lib/tooba/blob.ex
    <<<<<<< SEARCH
    defmodule Tooba.Blob.Store do
      @moduledoc """
      A hash-based content-addressable store for binary data.
      stored in the file system.
      """
    end
    =======
    defmodule Tooba.Blob.Store do
      @moduledoc """
      A hash-based content-addressable store for binary data,
      stored in the file system within the user data directory.
      """
    
      # Helper function to ensure the storage directory exists.
      defp ensure_data_dir_exists do
        data_home = get_xdg_data_home()
        :ok = File.mkdir_p(data_home)
      end
    
      # Helper function to generate file paths.
      defp blob_store_file_path(file_name) do
        Path.join([get_xdg_data_home(), file_name])
      end
    
      # Returns the XDG data home path.
      defp get_xdg_data_home do
        :filename.basedir(:user_data, Application.get_application(:tooba) |> Atom.to_string())
      end
    
      # Saves a blob to the file system.
      def save_blob(file_name, data) do
        ensure_data_dir_exists()
        file_path = blob_store_file_path(file_name)
    
        File.write(file_path, data)
      end
    
      # Retrieves a blob from the file system.
      def get_blob(file_name) do
        file_path = blob_store_file_path(file_name)
    
        if File.exists?(file_path) do
          File.read(file_path)
        else
          {:error, :not_found}
        end
      end
    end
    >>>>>>> REPLACE
    ```
    
    This code adds two private helper functions, `ensure_data_dir_exists` and `blob_store_file_path`, which are similar to those in `Tooba.RDF.Store`. It also adds two public functions, `save_blob/2` and `get_blob/1`, to save and retrieve blobs from the file system. The `save_blob/2` function ensures that the data directory exists before writing the file, and `get_blob/1` checks for the file's existence before attempting to read it.

--- lib/tooba/blob.ex
+++ lib/tooba/blob.ex
@@ -1,6 +1,41 @@
 defmodule Tooba.Blob.Store do
   @moduledoc """
-  A hash-based content-addressable store for binary data.
-  stored in the file system.
+  A hash-based content-addressable store for binary data,
+  stored in the file system within the user data directory.
   """
+
+  # Helper function to ensure the storage directory exists.
+  defp ensure_data_dir_exists do
+    data_home = get_xdg_data_home()
+    :ok = File.mkdir_p(data_home)
+  end
+
+  # Helper function to generate file paths.
+  defp blob_store_file_path(file_name) do
+    Path.join([get_xdg_data_home(), file_name])
+  end
+
+  # Returns the XDG data home path.
+  defp get_xdg_data_home do
+    :filename.basedir(:user_data, Application.get_application(:tooba) |> Atom.to_string())
+  end
+
+  # Saves a blob to the file system.
+  def save_blob(file_name, data) do
+    ensure_data_dir_exists()
+    file_path = blob_store_file_path(file_name)
+
+    File.write(file_path, data)
+  end
+
+  # Retrieves a blob from the file system.
+  def get_blob(file_name) do
+    file_path = blob_store_file_path(file_name)
+
+    if File.exists?(file_path) do
+      File.read(file_path)
+    else
+      {:error, :not_found}
+    end
+  end
 end
